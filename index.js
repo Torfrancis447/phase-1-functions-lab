e55c2970c2d09397175d889ab06bab1d0978b53c
tor <torfrancis447@gmail.com>
2022-11-01 11:00:37 -0400

    commit tree 6b1382d4ae49a6f28358676d14f320cccf1728d8

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(currentLocation) {
+  //return Math.abs(42 - location)
+//}
+    if (currentLocation < 42)
+      return 42 - currentLocation;
+
+    else (currentLocation > 42)
+      return currentLocation - 42
+  }
+
+
+function distanceFromHqInFeet(blocks) {
+  return distanceFromHqInBlocks(blocks) * 264;
+
+ }
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance <= 400) {
+  return(0);
+  }
+  else if (distance > 400 && distance < 2000) {
+  return (distance - 400) * .02 
+  }
+  else if (distance >= 2000 && distance <= 2500){
+  return (25)
+  }
+  else if (distance > 2500){
+  return ('cannot travel that far')
+  }
+}
+  
+
+
+    // function scuberGreetingForFeet(distance){
+  
+    //   if (distance <= 199) {
+    //     return ('This one is on me!')
+    //   } else if (distance > 400 && distance < 2000) {
+    //     return ('That will be twenty bucks.')
+    //   } else if (distance > 2000 && distance <= 2500 ) {
+    //     return ('I will gladly take your thirty bucks.')
+    //   } else if (distance > 2500 ) {
+    //     return('No can do.')
+    //   }
